ef1f1af68895c726b039d2342f8b7ab2c8c9f6fc
ynouelati <135117077+ynouelati@users.noreply.github.com>
2023-06-15 10:16:20 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import {defineConfig} from 'vite'
+// import {defineConfig} from 'vite'
 
 
-export default defineConfig({
-	plugins: []	
-})
+// export default defineConfig({
+// 	plugins: []	
+// })
